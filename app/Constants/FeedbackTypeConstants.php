348e75a661d4322f3db75250e7663461e4113e45
mustafa <mustafaguner235@gmail.com>
2024-10-08 23:57:28 +0300

    add feedback types and email refactor

---
+++ app/Constants/FeedbackTypeConstants.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Constants;
+
+class FeedbackTypeConstants
+{
+    const BUG = 1;
+    const SUGGESTION = 2;
+    const COMPLAINT = 3;
+    const FEATURE = 4;
+    const OTHER = 5;
+}
